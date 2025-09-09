71d79486c7fc04affa4d0e1db65af0b49848e7aa
Kevin Dev <kevin.developer71@gmail.com>
2025-09-09 19:21:05 -0400

    chore(9-frontend-setup-react-testing-library-and-msw): setup react testing library and msw server

---
+++ .jest/setEnvVars.ts
@@ -0,0 +1,1 @@
+process.env.NEXT_PUBLIC_API_HOST = "http://test-api.test"
---
+++ jest.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "jest"
+import nextJest from "next/jest.js"
+
+const createJestConfig = nextJest({
+  dir: "./",
+})
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+  testEnvironmentOptions: {
+    customExportConditions: [""],
+  },
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFiles: ["<rootDir>/jest.polyfills.js", "<rootDir>/.jest/setEnvVars.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+    "\\.(scss|sass|css)$": "identity-obj-proxy",
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/.next/", "/out/", "/build/"],
+  transformIgnorePatterns: ["/node_modules/"],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/.next/",
+    "jest.config.ts",
+    "next.config.ts",
+    "/src/config/msw/",
+    ".*types\\.ts$",
+  ],
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config)
